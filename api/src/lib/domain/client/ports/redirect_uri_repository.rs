412a405978903a916f0796bb2c73fc9d4e8785af
ZUHOWKS <jojo.vilard38@gmail.com>
2025-04-19 18:03:05 +0200

    Refactor RedirectUriRepository and RedirectUriService to use RedirectUriError instead of ClientError

--- api/src/lib/domain/client/ports/redirect_uri_repository.rs
+++ api/src/lib/domain/client/ports/redirect_uri_repository.rs
@@ -1,7 +1,9 @@
 use std::future::Future;
 use uuid::Uuid;
 
-use crate::domain::client::entities::{error::ClientError, redirect_uri::RedirectUri};
+use crate::domain::client::entities::{
+    redirect_uri::RedirectUri, redirect_uri_error::RedirectUriError,
+};
 
 pub trait RedirectUriRepository: Clone + Send + Sync + 'static {
     fn create_redirect_uri(
@@ -9,23 +11,23 @@ pub trait RedirectUriRepository: Clone + Send + Sync + 'static {
         client_id: Uuid,
         value: String,
         enabled: bool,
-    ) -> impl Future<Output = Result<RedirectUri, ClientError>> + Send;
+    ) -> impl Future<Output = Result<RedirectUri, RedirectUriError>> + Send;
 
     fn get_by_client_id(
         &self,
         client_id: Uuid,
-    ) -> impl Future<Output = Result<Vec<RedirectUri>, ClientError>> + Send;
+    ) -> impl Future<Output = Result<Vec<RedirectUri>, RedirectUriError>> + Send;
 
     fn get_enabled_by_client_id(
         &self,
         client_id: Uuid,
-    ) -> impl Future<Output = Result<Vec<RedirectUri>, ClientError>> + Send;
+    ) -> impl Future<Output = Result<Vec<RedirectUri>, RedirectUriError>> + Send;
 
     fn update_enabled(
         &self,
         id: Uuid,
         enabled: bool,
-    ) -> impl Future<Output = Result<RedirectUri, ClientError>> + Send;
+    ) -> impl Future<Output = Result<RedirectUri, RedirectUriError>> + Send;
 
-    fn delete(&self, id: Uuid) -> impl Future<Output = Result<(), ClientError>> + Send;
+    fn delete(&self, id: Uuid) -> impl Future<Output = Result<(), RedirectUriError>> + Send;
 }
--- api/src/lib/domain/client/ports/redirect_uri_service.rs
+++ api/src/lib/domain/client/ports/redirect_uri_service.rs
@@ -1,30 +1,32 @@
 use std::future::Future;
 use uuid::Uuid;
 
-use crate::domain::client::entities::{error::ClientError, redirect_uri::RedirectUri};
+use crate::domain::client::entities::{
+    redirect_uri::RedirectUri, redirect_uri_error::RedirectUriError,
+};
 
 pub trait RedirectUriService: Clone + Send + Sync + 'static {
     fn add_redirect_uri(
         &self,
         client_id: Uuid,
         uri: String,
-    ) -> impl Future<Output = Result<RedirectUri, ClientError>> + Send;
+    ) -> impl Future<Output = Result<RedirectUri, RedirectUriError>> + Send;
 
     fn get_by_client_id(
         &self,
         client_id: Uuid,
-    ) -> impl Future<Output = Result<Vec<RedirectUri>, ClientError>> + Send;
+    ) -> impl Future<Output = Result<Vec<RedirectUri>, RedirectUriError>> + Send;
 
     fn get_enabled_by_client_id(
         &self,
         client_id: Uuid,
-    ) -> impl Future<Output = Result<Vec<RedirectUri>, ClientError>> + Send;
+    ) -> impl Future<Output = Result<Vec<RedirectUri>, RedirectUriError>> + Send;
 
     fn update_enabled(
         &self,
         id: Uuid,
         enabled: bool,
-    ) -> impl Future<Output = Result<RedirectUri, ClientError>> + Send;
+    ) -> impl Future<Output = Result<RedirectUri, RedirectUriError>> + Send;
 
-    fn delete(&self, id: Uuid) -> impl Future<Output = Result<(), ClientError>> + Send;
+    fn delete(&self, id: Uuid) -> impl Future<Output = Result<(), RedirectUriError>> + Send;
 }
