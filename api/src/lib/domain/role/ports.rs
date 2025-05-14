d00489f97d15d051fbd2e199bf11684b4e2318b9
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2025-05-14 22:47:29 +0200

    feat: Add SeaORM entities for redirect URIs, refresh tokens, roles, user roles, user sessions, and users
    - Implemented `redirect_uris`, `refresh_tokens`, `roles`, `user_role`, `user_sessions`, and `users` entities with their respective models, columns, and relationships.
    - Updated migrations to create corresponding tables with appropriate data types and constraints.
    - Modified server initialization to include database connection for realm repository.
    - Added role repository for handling role-related database operations.
    - Refactored realm repository to utilize SeaORM for database interactions.

--- api/src/lib/domain/role/ports.rs
+++ api/src/lib/domain/role/ports.rs
@@ -1,11 +1,12 @@
 use uuid::Uuid;
 
-use super::entities::{errors::RoleError, CreateRoleRequest, Role};
-
-
+use super::entities::{CreateRoleRequest, errors::RoleError, models::Role};
 
 pub trait RoleService: Send + Sync {
-    fn create(&self, payload: CreateRoleRequest) -> impl Future<Output = Result<Role, RoleError>> + Send;
+    fn create(
+        &self,
+        payload: CreateRoleRequest,
+    ) -> impl Future<Output = Result<Role, RoleError>> + Send;
     fn get_by_realm_id(
         &self,
         realm_id: Uuid,
@@ -19,14 +20,24 @@ pub trait RoleService: Send + Sync {
         client_id: String,
         realm_id: Uuid,
     ) -> impl Future<Output = Result<Vec<Role>, RoleError>> + Send;
-    fn get_by_id(
-        &self,
-        id: Uuid
-    ) -> impl Future<Output = Result<Role, RoleError>> + Send;
-    fn delete_by_id(
-        &self,
-        id: Uuid
-    ) -> impl Future<Output = Result<(), RoleError>> + Send;
+    fn get_by_id(&self, id: Uuid) -> impl Future<Output = Result<Role, RoleError>> + Send;
+    fn delete_by_id(&self, id: Uuid) -> impl Future<Output = Result<(), RoleError>> + Send;
 }
 
-pub trait RoleRepository: Send + Sync {}
+pub trait RoleRepository: Send + Sync {
+    fn create(
+        &self,
+        payload: CreateRoleRequest,
+    ) -> impl Future<Output = Result<(), RoleError>> + Send;
+    fn get_by_client_id(
+        &self,
+        client_id: Uuid,
+    ) -> impl Future<Output = Result<Vec<Role>, RoleError>> + Send;
+    fn get_by_client_id_text(
+        &self,
+        client_id: String,
+        realm_id: Uuid,
+    ) -> impl Future<Output = Result<Vec<Role>, RoleError>> + Send;
+    fn get_by_id(&self, id: Uuid) -> impl Future<Output = Result<Option<Role>, RoleError>> + Send;
+    fn delete_by_id(&self, id: Uuid) -> impl Future<Output = Result<(), RoleError>> + Send;
+}
