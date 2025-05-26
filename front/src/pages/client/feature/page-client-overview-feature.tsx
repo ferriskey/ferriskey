f5b87381a8ab51880ca77844093560972d2d7719
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2025-05-27 00:03:55 +0200

    feat(client): add client overview feature and API integration
    - Introduced GetClientResponse interface for fetching client data.
    - Implemented useGetClient hook to retrieve client details based on realm and client ID.
    - Created ClientLayout component to display client information and tabs for settings.
    - Developed PageClientOverview component to show general settings of the client.
    - Updated routing to include client overview paths and integrated new components.
    - Enhanced app sidebar and navigation to reflect changes in client management.
    - Added React Query Devtools for improved debugging during development.

--- front/src/pages/client/feature/page-client-overview-feature.tsx
+++ front/src/pages/client/feature/page-client-overview-feature.tsx
@@ -1,10 +1,20 @@
+import { useGetClient } from "@/api/client.api"
+import { RouterParams } from "@/routes/router"
+import { useParams } from "react-router"
+import PageClientOverview from "../ui/page-client-overview"
+
 export default function PageClientOverviewFeature() {
+  const { realm_name, client_id } = useParams<RouterParams>()
+  const { data } = useGetClient({
+    realm: realm_name ?? 'master',
+    clientId: client_id ?? '',
+  })
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Client Overview</h1>
-      <p>This is the overview page for a specific client.</p>
-      {/* Add more content or components as needed */}
-    </div>
+    <>
+      {data && (
+        <PageClientOverview client={data} />
+      )}
+    </>
   )
 }
--- front/src/pages/client/page-client.tsx
+++ front/src/pages/client/page-client.tsx
@@ -2,15 +2,18 @@ import { Route, Routes } from "react-router";
 import PageClientsOverviewFeature from "./feature/page-clients-overview-feature";
 import Container from "./container";
 import PageClientOverviewFeature from "./feature/page-client-overview-feature";
+import ClientLayout from "./layout/client-layout";
 
 export default function PageClient() {
   return (
     <Routes>
       <Route element={<Container />}>
         <Route path="/overview" element={<PageClientsOverviewFeature />} />
-
-        <Route path="/:client_id/overview" element={<PageClientOverviewFeature />} />
       </Route>    
+
+      <Route element={<ClientLayout />}>
+        <Route path="/:client_id/overview" element={<PageClientOverviewFeature />} />
+      </Route>
     </Routes>
   )
 }
