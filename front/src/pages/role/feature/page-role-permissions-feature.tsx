f2c4109b9f21325ebc4f66f2e6174e3afa3385c3
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-07-21 19:26:30 +0200

    feat: implement role permissions form handling

--- front/src/pages/role/feature/page-role-permissions-feature.tsx
+++ front/src/pages/role/feature/page-role-permissions-feature.tsx
@@ -1,7 +1,11 @@
-import { useParams } from 'react-router'
-import { RouterParams } from '@/routes/router.ts'
 import { useGetRole } from '@/api/role.api.ts'
 import PageRolePermissions from '@/pages/role/ui/page-role-permissions.tsx'
+import { RouterParams } from '@/routes/router.ts'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { useParams } from 'react-router'
+import { Form } from '../../../components/ui/form'
+import { updateRolePermissionsSchema, UpdateRolePermissionsSchema } from '../schemas/update-role.schema'
 
 export default function PageRolePermissionsFeature() {
   const { realm_name, role_id } = useParams<RouterParams>()
@@ -11,9 +15,30 @@ export default function PageRolePermissionsFeature() {
     roleId: role_id,
   })
 
+  const form = useForm<UpdateRolePermissionsSchema>({
+    resolver: zodResolver(updateRolePermissionsSchema),
+    values: {
+      permissions: role?.permissions ?? [],
+    },
+  })
+
+  function togglePermission(permission: string) {
+    const currentPermissions = form.getValues('permissions') || [];
+    const copy = currentPermissions.includes(permission)
+      ? currentPermissions.filter((p) => p !== permission)
+      : [...currentPermissions, permission];
+
+    form.setValue('permissions', copy, { shouldDirty: true });
+  }
+
   if (!role) return null
 
   return (
-    <PageRolePermissions role={role} />
+    <Form {...form}>
+      <PageRolePermissions
+        role={role}
+        togglePermission={togglePermission}
+      />
+    </Form>
   )
 }
--- front/src/pages/role/ui/page-role-permissions.tsx
+++ front/src/pages/role/ui/page-role-permissions.tsx
@@ -1,16 +1,30 @@
 import { Role } from '@/api/api.interface.ts'
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Shield, Users } from 'lucide-react'
-import { permissionGroups } from '@/pages/role/types/permission-groups.ts'
 import BadgeColor, { BadgeColorScheme } from '@/components/ui/badge-color.tsx'
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { cn } from '@/lib/utils.ts'
+import { permissionGroups } from '@/pages/role/types/permission-groups.ts'
+import { Shield, Users } from 'lucide-react'
+import { useFormContext } from 'react-hook-form'
+import { UpdateRolePermissionsSchema } from '../schemas/update-role.schema'
 
 export interface PageRolePermissionsProps {
   role: Role
+  togglePermission: (permission: string) => void
 }
 
-export default function PageRolePermissions({ role }: PageRolePermissionsProps) {
-  const permissions = role.permissions || []
+export default function PageRolePermissions(props: PageRolePermissionsProps) {
+  const { role, togglePermission } = props
+
+  const { watch } = useFormContext<UpdateRolePermissionsSchema>()
+  const permissions = watch('permissions')
+
+  function normalizePermissionName(permission: string) {
+    return permission
+      .toString()
+      .split('_')
+      .map((p) => p.charAt(0).toUpperCase() + p.slice(1))
+      .join(' ')
+  }
 
   return (
     <div className="space-y-6">
@@ -20,7 +34,9 @@ export default function PageRolePermissions({ role }: PageRolePermissionsProps) 
             <div className="flex items-center justify-center h-10 w-10 rounded-full bg-primary/10">
               <Shield className="h-5 w-5 text-primary" />
             </div>
-            <CardTitle className="text-xl font-bold">{role.name}</CardTitle>
+            <CardTitle className="text-xl font-bold">
+              {role.name} {permissions.length}
+            </CardTitle>
           </div>
           <CardDescription className="mt-1">
             {role.description || 'No description provided'}
@@ -49,21 +65,17 @@ export default function PageRolePermissions({ role }: PageRolePermissionsProps) 
               </div>
               <div className="flex flex-col gap-3 mt-4">
                 {groupPermissions.map((permission) => {
-                  const permissionName = permission
-                    .toString()
-                    .split('_')
-                    .map((p) => p.charAt(0).toUpperCase() + p.slice(1))
-                    .join(' ')
                   const inRolePermissions = permissions.includes(permission.toString())
                   return (
                     <div
                       className={cn(
-                        'p-2 border rounded-sm',
+                        'p-2 border rounded-sm cursor-pointer',
                         inRolePermissions
                           ? 'bg-primary/10 hover:bg-primary/20'
                           : 'bg-muted/10 hover:bg-muted/20'
                       )}
                       key={permission}
+                      onClick={() => togglePermission(permission)}
                     >
                       <div className="flex items-center gap-2">
                         <span
@@ -74,7 +86,7 @@ export default function PageRolePermissions({ role }: PageRolePermissionsProps) 
                               : 'text-muted-foreground'
                           )}
                         >
-                          {permissionName}
+                          {normalizePermissionName(permission)}
                         </span>
                         <div>
                           <BadgeColor
