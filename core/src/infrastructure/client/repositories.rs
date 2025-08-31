3e2ee04b37a30d90a6106e4c35e0f33d2136beeb
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-09-01 01:28:41 +0200

    feat: split client mappers into separate modules

--- core/src/infrastructure/client/repositories.rs
+++ core/src/infrastructure/client/repositories.rs
@@ -3,10 +3,62 @@ use crate::domain::client::entities::{Client, ClientError};
 use crate::domain::client::ports::{ClientRepository, RedirectUriRepository};
 use crate::domain::client::value_objects::{CreateClientRequest, UpdateClientRequest};
 use crate::infrastructure::client::repositories::client_postgres_repository::PostgresClientRepository;
-use crate::infrastructure::repositories::redirect_uri_repository::PostgresRedirectUriRepository;
+use crate::infrastructure::client::repositories::redirect_uri_postgres_repository::PostgresRedirectUriRepository;
 use uuid::Uuid;
 
 pub mod client_postgres_repository;
+pub mod redirect_uri_postgres_repository;
+
+#[derive(Clone)]
+pub enum ClientRepoAny {
+    Postgres(PostgresClientRepository),
+}
+
+impl ClientRepository for ClientRepoAny {
+    async fn create_client(&self, data: CreateClientRequest) -> Result<Client, ClientError> {
+        match self {
+            Self::Postgres(repo) => repo.create_client(data).await,
+        }
+    }
+
+    async fn get_by_client_id(
+        &self,
+        client_id: String,
+        realm_id: Uuid,
+    ) -> Result<Client, ClientError> {
+        match self {
+            Self::Postgres(repo) => repo.get_by_client_id(client_id, realm_id).await,
+        }
+    }
+
+    async fn get_by_id(&self, id: Uuid) -> Result<Client, ClientError> {
+        match self {
+            Self::Postgres(repo) => repo.get_by_id(id).await,
+        }
+    }
+
+    async fn get_by_realm_id(&self, realm_id: Uuid) -> Result<Vec<Client>, ClientError> {
+        match self {
+            Self::Postgres(repo) => repo.get_by_realm_id(realm_id).await,
+        }
+    }
+
+    async fn update_client(
+        &self,
+        client_id: Uuid,
+        data: UpdateClientRequest,
+    ) -> Result<Client, ClientError> {
+        match self {
+            Self::Postgres(repo) => repo.update_client(client_id, data).await,
+        }
+    }
+
+    async fn delete_by_id(&self, id: Uuid) -> Result<(), ClientError> {
+        match self {
+            Self::Postgres(repo) => repo.delete_by_id(id).await,
+        }
+    }
+}
 
 #[derive(Clone)]
 pub enum RedirectUriRepoAny {
@@ -58,57 +110,6 @@ impl RedirectUriRepository for RedirectUriRepoAny {
     async fn delete(&self, id: Uuid) -> Result<(), RedirectUriError> {
         match self {
             RedirectUriRepoAny::Postgres(repo) => repo.delete(id).await,
-        }
-    }
-}
-
-#[derive(Clone)]
-pub enum ClientRepoAny {
-    Postgres(PostgresClientRepository),
-}
-
-impl ClientRepository for ClientRepoAny {
-    async fn create_client(&self, data: CreateClientRequest) -> Result<Client, ClientError> {
-        match self {
-            Self::Postgres(repo) => repo.create_client(data).await,
-        }
-    }
-
-    async fn get_by_client_id(
-        &self,
-        client_id: String,
-        realm_id: Uuid,
-    ) -> Result<Client, ClientError> {
-        match self {
-            Self::Postgres(repo) => repo.get_by_client_id(client_id, realm_id).await,
-        }
-    }
-
-    async fn get_by_id(&self, id: Uuid) -> Result<Client, ClientError> {
-        match self {
-            Self::Postgres(repo) => repo.get_by_id(id).await,
-        }
-    }
-
-    async fn get_by_realm_id(&self, realm_id: Uuid) -> Result<Vec<Client>, ClientError> {
-        match self {
-            Self::Postgres(repo) => repo.get_by_realm_id(realm_id).await,
-        }
-    }
-
-    async fn update_client(
-        &self,
-        client_id: Uuid,
-        data: UpdateClientRequest,
-    ) -> Result<Client, ClientError> {
-        match self {
-            Self::Postgres(repo) => repo.update_client(client_id, data).await,
-        }
-    }
-
-    async fn delete_by_id(&self, id: Uuid) -> Result<(), ClientError> {
-        match self {
-            Self::Postgres(repo) => repo.delete_by_id(id).await,
         }
     }
 }
