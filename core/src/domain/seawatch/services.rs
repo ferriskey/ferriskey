ded9d9e65f5cc1f8e02ee20abc237537a7efbd06
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2025-11-25 09:26:49 +0100

    feat: implement SecurityEventServiceImpl and fetch_events

--- core/src/domain/seawatch/services.rs
+++ core/src/domain/seawatch/services.rs
@@ -1,7 +1,13 @@
+use std::sync::Arc;
+
 use crate::domain::{
     authentication::{ports::AuthSessionRepository, value_objects::Identity},
     client::ports::{ClientRepository, RedirectUriRepository},
-    common::{entities::app_errors::CoreError, policies::ensure_policy, services::Service},
+    common::{
+        entities::app_errors::CoreError,
+        policies::{FerriskeyPolicy, ensure_policy},
+        services::Service,
+    },
     credential::ports::CredentialRepository,
     crypto::ports::HasherRepository,
     health::ports::HealthCheckRepository,
@@ -35,6 +41,59 @@ where
     W: WebhookRepository,
     RT: RefreshTokenRepository,
     RC: RecoveryCodeRepository,
+    SE: SecurityEventRepository,
+{
+    async fn fetch_events(
+        &self,
+        identity: Identity,
+        input: FetchEventsInput,
+    ) -> Result<Vec<SecurityEvent>, CoreError> {
+        let realm = self
+            .realm_repository
+            .get_by_name(input.realm_name)
+            .await
+            .map_err(|_| CoreError::InvalidRealm)?
+            .ok_or(CoreError::InvalidRealm)?;
+
+        let realm_id = realm.id;
+        ensure_policy(
+            self.policy.can_view_events(identity, realm).await,
+            "insufficient permissions",
+        )?;
+
+        let security_events = self
+            .security_event_repository
+            .get_events(
+                realm_id,
+                SecurityEventFilter {
+                    ..Default::default()
+                },
+            )
+            .await?;
+
+        Ok(security_events)
+    }
+}
+
+pub struct SecurityEventServiceImpl<R, U, C, UR, SE>
+where
+    R: RealmRepository,
+    U: UserRepository,
+    C: ClientRepository,
+    UR: UserRoleRepository,
+    SE: SecurityEventRepository,
+{
+    pub(crate) realm_repository: Arc<R>,
+    pub(crate) security_event_repository: Arc<SE>,
+    pub(crate) policy: Arc<FerriskeyPolicy<U, C, UR>>,
+}
+
+impl<R, U, C, UR, SE> SecurityEventService for SecurityEventServiceImpl<R, U, C, UR, SE>
+where
+    R: RealmRepository,
+    U: UserRepository,
+    C: ClientRepository,
+    UR: UserRoleRepository,
     SE: SecurityEventRepository,
 {
     async fn fetch_events(
