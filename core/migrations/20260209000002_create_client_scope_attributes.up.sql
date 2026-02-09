7ac943c197b696d30a6f37ee841898e16eec31e1
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2026-02-09 22:32:44 +0100

    wip

--- core/migrations/20260209000002_create_client_scope_attributes.up.sql
+++ core/migrations/20260209000002_create_client_scope_attributes.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE client_scope_attributes (
     id UUID PRIMARY KEY,
-    scope_id UUID NOT NULL REFERENCES client_scopes(id) ON DELETE CASCADE,
+    client_scope_id UUID NOT NULL REFERENCES client_scopes(id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     value VARCHAR(2048),
     CONSTRAINT unique_attribute_per_scope UNIQUE (scope_id, name)
