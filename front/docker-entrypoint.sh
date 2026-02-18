75d3c9cfc61cc598a6a87f4e508290bb4fdf90f7
nazrulkhan <nazrulkhan.dk@gmail.com>
2026-02-18 16:34:30 +0600

    fix(webapp): escape sed delimiter in API_URL replacement

--- front/docker-entrypoint.sh
+++ front/docker-entrypoint.sh
@@ -11,7 +11,7 @@ cp -r "$SRC_DIR"/* "$HTML_DIR"
 
 if [ -f "$CONFIG_FILE" ]; then
   api_url="${API_URL:-}"
-  escaped_api_url=$(printf '%s' "$api_url" | sed -e 's/[\/&]/\\&/g')
+  escaped_api_url=$(printf '%s' "$api_url" | sed -e 's/[\/&|]/\\&/g')
   # shellcheck disable=SC2016
   placeholder='${API_URL}'
   sed -i "s|$placeholder|$escaped_api_url|g" "$CONFIG_FILE"
