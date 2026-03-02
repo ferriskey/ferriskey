f9b07726d176896e3484fa4b2bf01433b34c4ed5
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2026-03-02 11:22:52 +0100

    fix: set default_scope_type based on is_default

--- core/migrations/20260225130422_update_client_scopes.up.sql
+++ core/migrations/20260225130422_update_client_scopes.up.sql
@@ -3,7 +3,11 @@ ALTER TABLE client_scopes
 ADD COLUMN IF NOT EXISTS "default_scope_type" VARCHAR(255) DEFAULT 'NONE' NOT NULL;
 
 -- Migrate existing data before dropping
-UPDATE client_scopes SET default_scope_type = 'DEFAULT' WHERE is_default = TRUE;
+UPDATE client_scopes
+SET default_scope_type = CASE
+    WHEN COALESCE(is_default, FALSE) = TRUE THEN 'DEFAULT'
+    ELSE 'OPTIONAL'
+END;
 
 -- remove the column "is_default" if it exists
 ALTER TABLE client_scopes
