3da3a3dff1dac737e7f566fc73a47223416db8c1
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2026-07-29 00:15:28 +0200

    refactor: extract maintenance http feature into ferriskey-api-maintenance (#1197)

--- libs/ferriskey-api-maintenance/src/handlers/remove_realm_whitelist_entry.rs
+++ libs/ferriskey-api-maintenance/src/handlers/remove_realm_whitelist_entry.rs
@@ -1,10 +1,10 @@
-use crate::application::http::server::api_entities::api_error::{ApiError, ApiErrorResponse};
-use crate::application::http::server::api_entities::response::Response;
-use crate::application::http::server::app_state::AppState;
 use axum::{
     Extension,
     extract::{Path, State},
 };
+use ferriskey_api_core::api_entities::api_error::{ApiError, ApiErrorResponse};
+use ferriskey_api_core::api_entities::response::Response;
+use ferriskey_api_core::app_state::AppState;
 use ferriskey_core::domain::authentication::value_objects::Identity;
 use ferriskey_core::domain::maintenance::ports::MaintenanceService;
 use serde::{Deserialize, Serialize};
