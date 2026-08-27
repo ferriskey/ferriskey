e273bab3a6a7598216a5be4a7c060c2c570c025d
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2026-08-27 14:52:14 +0200

    feat: render the realm saml metadata descriptor

--- libs/ferriskey-saml/src/metadata.rs
+++ libs/ferriskey-saml/src/metadata.rs
@@ -1,5 +1,12 @@
-use crate::authn::NameIdFormat;
+use crate::authn::{NameIdFormat, ProtocolBinding};
 use thiserror::Error;
+
+const METADATA_NAMESPACE: &str = "urn:oasis:names:tc:SAML:2.0:metadata";
+const XMLDSIG_NAMESPACE: &str = "http://www.w3.org/2000/09/xmldsig#";
+const PROTOCOL_NAMESPACE: &str = "urn:oasis:names:tc:SAML:2.0:protocol";
+const SIGNING_KEY_USE: &str = "signing";
+const SINGLE_SIGN_ON_BINDINGS: [ProtocolBinding; 2] =
+    [ProtocolBinding::HttpRedirect, ProtocolBinding::HttpPost];
 
 #[derive(Debug, Error, PartialEq, Eq)]
 pub enum MetadataError {
@@ -18,15 +25,134 @@ pub struct IdpMetadataDescriptor {
     pub signing_certificate_base64_der: String,
 }
 
-pub fn render_idp_metadata(_descriptor: &IdpMetadataDescriptor) -> Result<String, MetadataError> {
-    Ok(concat!(
-        r#"<md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" entityID="https://auth.example.com/realms/master">"#,
-        r#"<md:IDPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol" WantAuthnRequestsSigned="false">"#,
-        r#"<md:KeyDescriptor use="signing"><ds:KeyInfo><ds:X509Data><ds:X509Certificate>MIIC</ds:X509Certificate></ds:X509Data></ds:KeyInfo></md:KeyDescriptor>"#,
-        r#"<md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="https://auth.example.com/realms/master/protocol/saml"/>"#,
-        r#"</md:IDPSSODescriptor></md:EntityDescriptor>"#,
-    )
-    .to_owned())
+pub fn render_idp_metadata(descriptor: &IdpMetadataDescriptor) -> Result<String, MetadataError> {
+    let entity_id = non_empty(&descriptor.entity_id, "entityID")?;
+    let single_sign_on_url = non_empty(
+        &descriptor.single_sign_on_url,
+        "SingleSignOnService/@Location",
+    )?;
+    let certificate = non_empty(
+        &descriptor.signing_certificate_base64_der,
+        "X509Certificate",
+    )?;
+
+    if descriptor.name_id_formats.is_empty() {
+        return Err(MetadataError::NoNameIdFormats);
+    }
+    let name_id_formats = descriptor
+        .name_id_formats
+        .iter()
+        .map(|format| non_empty(format.as_uri(), "NameIDFormat"))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let want_authn_requests_signed = if descriptor.want_authn_requests_signed {
+        "true"
+    } else {
+        "false"
+    };
+
+    let mut xml = String::new();
+
+    open_element(
+        &mut xml,
+        "md:EntityDescriptor",
+        &[("xmlns:md", METADATA_NAMESPACE), ("entityID", entity_id)],
+    );
+    open_element(
+        &mut xml,
+        "md:IDPSSODescriptor",
+        &[
+            ("WantAuthnRequestsSigned", want_authn_requests_signed),
+            ("protocolSupportEnumeration", PROTOCOL_NAMESPACE),
+        ],
+    );
+
+    open_element(&mut xml, "md:KeyDescriptor", &[("use", SIGNING_KEY_USE)]);
+    open_element(&mut xml, "ds:KeyInfo", &[("xmlns:ds", XMLDSIG_NAMESPACE)]);
+    open_element(&mut xml, "ds:X509Data", &[]);
+    text_element(&mut xml, "ds:X509Certificate", certificate);
+    close_element(&mut xml, "ds:X509Data");
+    close_element(&mut xml, "ds:KeyInfo");
+    close_element(&mut xml, "md:KeyDescriptor");
+
+    for format in name_id_formats {
+        text_element(&mut xml, "md:NameIDFormat", format);
+    }
+
+    for binding in SINGLE_SIGN_ON_BINDINGS {
+        open_element(
+            &mut xml,
+            "md:SingleSignOnService",
+            &[
+                ("Binding", binding.as_uri()),
+                ("Location", single_sign_on_url),
+            ],
+        );
+        close_element(&mut xml, "md:SingleSignOnService");
+    }
+
+    close_element(&mut xml, "md:IDPSSODescriptor");
+    close_element(&mut xml, "md:EntityDescriptor");
+
+    Ok(xml)
+}
+
+fn non_empty<'a>(value: &'a str, label: &'static str) -> Result<&'a str, MetadataError> {
+    if value.is_empty() {
+        return Err(MetadataError::EmptyValue(label));
+    }
+    Ok(value)
+}
+
+fn open_element(xml: &mut String, name: &str, attributes: &[(&str, &str)]) {
+    xml.push('<');
+    xml.push_str(name);
+    for (key, value) in attributes {
+        xml.push(' ');
+        xml.push_str(key);
+        xml.push_str("=\"");
+        push_escaped_attribute_value(xml, value);
+        xml.push('"');
+    }
+    xml.push('>');
+}
+
+fn close_element(xml: &mut String, name: &str) {
+    xml.push_str("</");
+    xml.push_str(name);
+    xml.push('>');
+}
+
+fn text_element(xml: &mut String, name: &str, text: &str) {
+    open_element(xml, name, &[]);
+    push_escaped_text(xml, text);
+    close_element(xml, name);
+}
+
+fn push_escaped_attribute_value(xml: &mut String, value: &str) {
+    for character in value.chars() {
+        match character {
+            '&' => xml.push_str("&amp;"),
+            '<' => xml.push_str("&lt;"),
+            '"' => xml.push_str("&quot;"),
+            '\t' => xml.push_str("&#x9;"),
+            '\n' => xml.push_str("&#xA;"),
+            '\r' => xml.push_str("&#xD;"),
+            other => xml.push(other),
+        }
+    }
+}
+
+fn push_escaped_text(xml: &mut String, text: &str) {
+    for character in text.chars() {
+        match character {
+            '&' => xml.push_str("&amp;"),
+            '<' => xml.push_str("&lt;"),
+            '>' => xml.push_str("&gt;"),
+            '\r' => xml.push_str("&#xD;"),
+            other => xml.push(other),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -82,9 +208,9 @@ mod tests {
     }
 
     #[test]
-    fn escapes_markup_significant_characters_in_configuration() {
+    fn escapes_markup_and_whitespace_in_configuration() {
         let descriptor = IdpMetadataDescriptor {
-            entity_id: r#"https://auth.example.com/realms/a&b?x="1"&y=<2>"#.to_owned(),
+            entity_id: "https://auth.example.com/realms/a&b?x=\"1\"&y=<2>\tz".to_owned(),
             single_sign_on_url: "https://auth.example.com/saml?a=1&b=2".to_owned(),
             ..reference_descriptor()
         };
@@ -94,7 +220,7 @@ mod tests {
 
         assert!(
             rendered.contains(
-                r#"entityID="https://auth.example.com/realms/a&amp;b?x=&quot;1&quot;&amp;y=&lt;2>""#
+                r#"entityID="https://auth.example.com/realms/a&amp;b?x=&quot;1&quot;&amp;y=&lt;2>&#x9;z""#
             ),
             "entity id was not escaped: {rendered}"
         );
