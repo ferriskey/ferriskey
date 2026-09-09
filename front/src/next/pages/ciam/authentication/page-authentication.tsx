ccb368dd9a2a2df8d56296356302f6bcdb446f80
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2026-09-09 19:55:47 +0200

    feat(front): build the console authentication section under /next
    Sign-in methods, Identity providers and Password policy. Sign-in methods
    and the password policy are console-owned features over the IAM realm
    tabs; identity providers mounts the IAM router as is.

--- front/src/next/pages/ciam/authentication/page-authentication.tsx
+++ front/src/next/pages/ciam/authentication/page-authentication.tsx
@@ -1,13 +1,15 @@
 import { Navigate, Route, Routes } from 'react-router'
-import ConsoleNotBuiltYet from '@/next/shell/ciam/not-built-yet'
+import NextPageIdentityProviders from '@/next/pages/iam/identity-providers/page-identity-providers'
+import PageSignInMethodsFeature from './feature/page-sign-in-methods-feature'
+import PagePasswordPolicyFeature from './feature/page-password-policy-feature'
 
 export default function ConsoleAuthentication() {
   return (
     <Routes>
       <Route index element={<Navigate to='sign-in-methods' replace />} />
-      <Route path='sign-in-methods' element={<ConsoleNotBuiltYet section='authentication' page='sign-in-methods' />} />
-      <Route path='identity-providers' element={<ConsoleNotBuiltYet section='authentication' page='identity-providers' />} />
-      <Route path='password-policy' element={<ConsoleNotBuiltYet section='authentication' page='password-policy' />} />
+      <Route path='sign-in-methods' element={<PageSignInMethodsFeature />} />
+      <Route path='identity-providers/*' element={<NextPageIdentityProviders />} />
+      <Route path='password-policy' element={<PagePasswordPolicyFeature />} />
     </Routes>
   )
 }
