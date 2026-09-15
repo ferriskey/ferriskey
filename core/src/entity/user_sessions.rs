4bf3eeb7ae286e1f0bd9622a424073fa7d5547e5
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2026-09-15 17:21:45 +0200

    chore(entity): add the relation codegen derives from the new foreign key

--- core/src/entity/user_sessions.rs
+++ core/src/entity/user_sessions.rs
@@ -51,6 +51,7 @@ impl PrimaryKeyTrait for PrimaryKey {
 
 #[derive(Copy, Clone, Debug, EnumIter)]
 pub enum Relation {
+    AuthSessions,
     Realms,
     Users,
 }
@@ -78,6 +79,7 @@ impl ColumnTrait for Column {
 impl RelationTrait for Relation {
     fn def(&self) -> RelationDef {
         match self {
+            Self::AuthSessions => Entity::has_many(super::auth_sessions::Entity).into(),
             Self::Realms => Entity::belongs_to(super::realms::Entity)
                 .from(Column::RealmId)
                 .to(super::realms::Column::Id)
@@ -87,6 +89,12 @@ impl RelationTrait for Relation {
                 .to(super::users::Column::Id)
                 .into(),
         }
+    }
+}
+
+impl Related<super::auth_sessions::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::AuthSessions.def()
     }
 }
 
